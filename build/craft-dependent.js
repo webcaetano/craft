8f56c3943572c4313c03da6378f7e9625ede0390
Andre <webandrecaetano@gmail.com>
2016-07-10 08:12:38 -0300

    fix circle

--- build/craft-dependent.js
+++ build/craft-dependent.js
@@ -381,6 +381,7 @@ return /******/ (function(modules) { // webpackBootstrap
 				group: undefined,
 				fill: '#ff0000',
 				size: 10,
+				alpha: 1,
 				stroke: {
 					size: 0,
 					color: '#000',
@@ -392,8 +393,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 			bindProto(tmpG, 'graphic');
 
-			if (options.fill) tmpG.beginFill(utils.colorCase(options.fill));
-			if (options.stroke.size > 0) tmpG.lineStyle(options.stroke.size, options.stroke.color, options.stroke.alpha);
+			if (options.fill) tmpG.beginFill(utils.colorCase(options.fill), options.alpha);
+			if (options.stroke.size > 0) tmpG.lineStyle(options.stroke.size, utils.colorCase(options.stroke.color), options.stroke.alpha);
 			return tmpG.drawCircle(0, 0, options.size);
 		};
 	};
