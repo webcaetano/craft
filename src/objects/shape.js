c2b19ed995ba15925ca9f38d6c91650fc639fbdd
Andre Caetano <webandrecaetano@gmail.com>
2016-08-11 00:40:49 -0300

    colorShape place in utils

--- src/objects/shape.js
+++ src/objects/shape.js
@@ -5,31 +5,6 @@ var Phaser = require('phaser');
 var {game} = require('../scope');
 var $sprite = require('./sprite');
 
-var colorShapeBmd = function(key, colorHex) {
-	var source = game.make.sprite(0,0,key);
-	var anchor = {x:source.anchor.x,y:source.anchor.y};
-	var scale = {x:source.scale.x,y:source.scale.y};
-
-	var color = Phaser.Color.hexToColor(colorHex);
-
-	source.anchor.set(0, 0);
-	source.scale.set(1, 1);
-
-	var texture = game.make.bitmapData(source.width, source.height);
-	texture.fill(color.r, color.g, color.b);
-
-	var bmd = texture;
-	bmd.blendDestinationAtop();
-	bmd.draw(source, 0, 0, source.texture.crop.width, source.texture.crop.height);
-	bmd.pendingDestroy = true;
-
-	source.anchor.set(anchor.x, anchor.y);
-	source.scale.set(scale.x, scale.y);
-	source.pendingDestroy = true;
-
-	return bmd;
-}
-
 module.exports = function $shape(source, colorHex='#FF0000', options){
 	if(typeof options == 'string') options = {frame:options};
 	var defaults = {
@@ -44,7 +19,7 @@ module.exports = function $shape(source, colorHex='#FF0000', options){
 	var key = '$fill_'+source.key+'_'+source.frameName;
 
 	if(options.cache && !game.cache.checkImageKey(key)){
-		var bmd = colorShapeBmd(source, colorHex);
+		var bmd = utils.colorShapeBmd(source, colorHex);
 		bmd.generateTexture(key);
 		bmd.pendingDestroy = true;
 	} else if(!options.cache){
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,36 @@
 var self = {};
 var Phaser = require('phaser');
+var {game} = require('./scope');
 
 self.colorCase = function(color){
 	return color.indexOf('#') != -1 ? '0x'+color.replace(/#/g,'') : color;
 }
 
 self.extend = Phaser.Utils.extend.bind(null,true) // always deep;
+
+self.colorShapeBmd = function(key, colorHex) {
+	var source = game.make.sprite(0,0,key);
+	var anchor = {x:source.anchor.x,y:source.anchor.y};
+	var scale = {x:source.scale.x,y:source.scale.y};
+
+	var color = Phaser.Color.hexToColor(colorHex);
+
+	source.anchor.set(0, 0);
+	source.scale.set(1, 1);
+
+	var texture = game.make.bitmapData(source.width, source.height);
+	texture.fill(color.r, color.g, color.b);
+
+	var bmd = texture;
+	bmd.blendDestinationAtop();
+	bmd.draw(source, 0, 0, source.texture.crop.width, source.texture.crop.height);
+	bmd.pendingDestroy = true;
+
+	source.anchor.set(anchor.x, anchor.y);
+	source.scale.set(scale.x, scale.y);
+	source.pendingDestroy = true;
+
+	return bmd;
+}
 
 module.exports = self;
