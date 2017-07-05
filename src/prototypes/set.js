629de0ac77b56178112e82add3f51578ba212cbf
Andre Caetano <webandrecaetano@gmail.com>
2017-07-05 14:22:12 -0300

    Revert "simplfy `$set`"
    This reverts commit b74eeddb568634560d18bbc4dbe2944b5f3ee340.

--- src/prototypes/set.js
+++ src/prototypes/set.js
@@ -2,7 +2,16 @@ module.exports = function $set(prop,val){
 	if(typeof prop==='string' && !!val){
 		this[prop]=val;
 	} else {
-		Object.assign(this,prop);
+		for(var i in prop){
+			if(i.indexOf('.')==-1){
+				this[i]=prop[i];
+			} else {
+				var pathObj = i.split(".");
+				var c = this;
+				for(var k=0;k<pathObj.length-1;k++) c=c[pathObj[k]];
+				c[pathObj[pathObj.length-1]]=prop[i];
+			}
+		}
 	}
 	return this;
 }
