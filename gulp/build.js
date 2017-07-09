cf3bbaad1bd9ff3b05ed16eb1bfaee1cce0c4cfd
Andre Caetano <webandrecaetano@gmail.com>
2017-07-09 12:54:56 -0300

    remove examples

--- gulp/build.js
+++ gulp/build.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')({
-	pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+	pattern: ['gulp-*', 'del']
 });
 
 module.exports = function(options) {
@@ -14,32 +14,6 @@ module.exports = function(options) {
 			}))
 			.pipe(gulp.dest(options.dist + '/'))
 			.pipe($.size({ title: options.dist + '/', showFiles: true }));
-	}));
-
-	gulp.task('copy:scripts:examples', function () {
-		return gulp.src(options.tmp+'/serve/**/*.js')
-		.pipe(gulp.dest('examples/'))
-	});
-
-	gulp.task('copy:others:examples', function () {
-		return gulp.src('test/**/*.{ico,png,jpg}')
-		.pipe(gulp.dest('examples/'))
-	});
-
-	gulp.task('html:examples', gulp.series('inject', function () {
-		var assets;
-		return gulp.src(options.tmp + '/serve/*.html')
-			.pipe(assets = $.useref.assets())
-			.pipe($.rev())
-			.pipe($.if('*.js', $.preprocess({context: {dist: true}})))
-			.pipe($.if('*.js', $.uglify()))
-			.pipe(assets.restore())
-			.pipe($.useref())
-			.pipe($.revReplace())
-			.pipe($.if('*.html', $.preprocess({context: {dist: true}})))
-			.pipe($.if('*.html', $.minifyHtml({empty: true,	spare: true, quotes: true, conditionals: true})))
-			.pipe(gulp.dest('examples/'))
-			.pipe($.size({ title: 'examples/', showFiles: true }));
 	}));
 
 	gulp.task('mincopy:js', function () {
@@ -60,11 +34,5 @@ module.exports = function(options) {
 		return $.del([options.dist + '/']);
 	});
 
-	gulp.task('clean:examples', function () {
-		return $.del(['examples/', options.tmp + '/']);
-	});
-
 	gulp.task('build',gulp.series('clean','clean:dist','build:js','mincopy:js'));
-	gulp.task('build:examples', gulp.series('clean','clean:examples','html:examples','copy:scripts:examples','copy:others:examples'));
-
 };
